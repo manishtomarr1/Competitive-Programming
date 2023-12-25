25c198f714967f715bd238d4d15514802f4710c5
manish tomar <tomarmanish32@gmail.com>
2023-12-25 21:04:26 +0530

    factor

--- loops/primeFactorization.cpp
+++ loops/primeFactorization.cpp
@@ -1,11 +1,33 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+vector<int> factor;
 
+void prime(int &num)
+{
+    int init = 2;
 
-int prime(){
+    while (num > 1)
+    {
+        while (num % init == 0)
+        {
+            num /= init;
+            factor.push_back(init);
+        }
 
+               init++;
+    }
 }
 
-int main () {
+int main()
+{
+    int num = 6;
+    prime(num);
+
+    for (auto &i : factor)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
     return 0;
 }
